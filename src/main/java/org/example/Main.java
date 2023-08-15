fc7f0f3c5bf4c92de7897fdec28a4fa4377ad91a
Сидоров Вадим Алексеевич <VASidorov@t1-consulting.ru>
2023-08-15 15:58:37 +0300

    sadsad

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a
+        int b = 11; int c = 8;
+        int h = 1000;
+        String email = "Test.ru"
+        int t = 11;
+        int b = "sajndjsandjsad"
+
+        int e = 8;
+        int f = 7;
+
+        public void bad(){
+
+            while(true){
+                System.out.println("bad");
+
+            }
+        }
+    }
+}
